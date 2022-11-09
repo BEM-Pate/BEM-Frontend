6037d81429b9ce6ba50919628344d62d50101420
Phillip Kiele <phillipkiele@gmail.com>
2022-11-09 02:05:55 +0100

    Add the disabled prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,13 @@ import styles from './Button.module.scss';
 
 interface Props {
   children?: React.ReactNode;
+  disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 const Button = (props: Props) => {
-  const { children, onClick = (() => {}) } = props;
-  return <button type="button" onClick={onClick} className={classNames(styles.Button)}>{children}</button>;
+  const { children, disabled, onClick = (() => {}) } = props;
+  return <button type="button" onClick={onClick} disabled={disabled} className={classNames(styles.Button)}>{children}</button>;
 };
 
 export default Button;
