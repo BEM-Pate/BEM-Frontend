cfeab445571b1d1b48559c0adb0bbfa453b6f412
Phillip Kiele <phillipkiele@gmail.com>
2022-11-09 02:02:57 +0100

    Refactor Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import classNames from 'classnames';
 import styles from './Button.module.scss';
 
-interface ButtonProps {
-  children: React.ReactNode,
+interface Props {
+  children?: React.ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = (props: ButtonProps) => {
-  const { children } = props;
-  return <button type="button" className={classNames(styles.Button)}>{children}</button>;
+const Button = (props: Props) => {
+  const { children, onClick = (() => {}) } = props;
+  return <button type="button" onClick={onClick} className={classNames(styles.Button)}>{children}</button>;
 };
 
 export default Button;
