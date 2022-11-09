21638e8e6af6d27fea4d2469645a3b2548d2e73c
f3lixh <47897037+f3lixh@users.noreply.github.com>
2022-11-09 22:51:04 +0100

    Refactor Textfield
    renamed placeholder, updated React.FC/props and colour variables

--- src/components/Textfield/Textfield.tsx
+++ src/components/Textfield/Textfield.tsx
@@ -2,28 +2,31 @@ import React, { HTMLInputTypeAttribute } from 'react';
 import classNames from 'classnames';
 import styles from './Textfield.module.scss';
 
-interface TextfieldProps {
-  children: string,
+interface Props {
+  placeholder: string,
   type: HTMLInputTypeAttribute,
   id: string,
   disabled?: boolean;
 }
 
-/* Label is hidden because there is no need to see the placeholder text twice */
-const Textfield: React.FC<TextfieldProps> = ({
-  children, type, id, disabled,
-}) => (
-  <>
-    <label className={classNames(styles.Label)} htmlFor={id}>{children}</label>
-    <input
-      placeholder={children}
-      className={classNames(styles.Input)}
-      type={type}
-      id={id}
-      name={id}
-      disabled={disabled}
-    />
-  </>
-);
+const Textfield = (props: Props) => {
+  const {
+    placeholder, type, id, disabled,
+  } = props;
+
+  return (
+    <>
+      <label hidden className={classNames(styles.Label)} htmlFor={id}>{placeholder}</label>
+      <input
+        placeholder={placeholder}
+        className={classNames(styles.Input)}
+        type={type}
+        id={id}
+        name={id}
+        disabled={disabled}
+      />
+    </>
+  );
+};
 
 export default Textfield;
