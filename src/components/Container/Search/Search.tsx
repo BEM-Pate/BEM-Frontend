3c2c7eb83407c7b8479db4db002206c144949024
f3lixh <47897037+f3lixh@users.noreply.github.com>
2022-12-07 21:14:07 +0100

    Pate SearchCanvas added

--- src/components/Container/Search/Search.tsx
+++ src/components/Container/Search/Search.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './Search.module.scss';
+import Textfield from '../../Textfield/Textfield';
+
+import placeholder from '../../../images/default.png';
+
+const GERMAN_FIRST_NAMES = ['Anna', 'Bruno', 'Carla', 'Dieter', 'Emma', 'Friedrich', 'Greta', 'Hans', 'Irina', 'Johann'];
+const GERMAN_LAST_NAMES = ['Müller', 'Schmidt', 'Schneider', 'Fischer', 'Weber', 'Meyer', 'Wagner', 'Becker', 'Schulz', 'Hoffmann'];
+const GERMAN_LOCATIONS = ['Berlin', 'Hamburg', 'München', 'Köln', 'Frankfurt', 'Stuttgart', 'Düsseldorf', 'Dortmund', 'Essen', 'Bremen'];
+
+const generateRandomPerson = () => {
+  const people = [];
+  while (people.length !== 50) {
+    const firstName = GERMAN_FIRST_NAMES[Math.floor(Math.random() * GERMAN_FIRST_NAMES.length)];
+    const lastName = GERMAN_LAST_NAMES[Math.floor(Math.random() * GERMAN_LAST_NAMES.length)];
+    const age = Math.floor(Math.random() * 100);
+    const location = GERMAN_LOCATIONS[Math.floor(Math.random() * GERMAN_LOCATIONS.length)];
+
+    const person = {
+      firstName,
+      lastName,
+      age,
+      location,
+    };
+
+    people.push(person);
+  }
+
+  return people;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const paten = generateRandomPerson();
 
 const Search = () => (
-  <div className={classNames(styles.Search)}>Search</div>
+  <div className={classNames(styles.Search)}>
+
+    <div>
+      <Textfield id="pate-search" type="text" placeholder="Search..." />
+    </div>
+    <div className={classNames(styles.PatePanel)}>
+      {paten.map((person, index) => (
+        <div key={index} className={classNames(styles.Pate)}>
+          <img src={placeholder} alt="placeholder" />
+          <div>
+            {person.firstName}
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 );
 
 export default Search;
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -3,9 +3,11 @@ import classNames from 'classnames';
 import { Outlet } from 'react-router-dom';
 import styles from './DashboardPage.module.scss';
 import BottomNavigtionBar from '../../components/BottomNavigationBar/BottomNavigtionBar';
+import TopNavigationBar from '../../components/TopNavigationBar/TopNavigationBar';
 
 const DashboardPage = () => (
   <div className={classNames(styles.Dashboard)}>
+    <TopNavigationBar />
     <Outlet />
     <BottomNavigtionBar />
   </div>
