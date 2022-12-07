236321be7fb14b6cd1029e23d960caf7d471db1e
Hasan Sivrikaya <35102813+Hasan-1994@users.noreply.github.com>
2022-12-07 22:58:24 +0100

    i18n for german and english
    Das Sprachpaket wurde für deutsch und englisch implementiert.

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import { useTranslation } from 'react-i18next';
 import styles from './LandingPage.module.scss';
 import Button from '../../components/Button/Button';
+import { availableLanguages } from '../../translation/i18n';
 
 interface LandingPageProps {
   children?: React.ReactNode
@@ -10,13 +12,24 @@ interface LandingPageProps {
 
 const LandingPage = (props: LandingPageProps) => {
   const { children } = props;
+  const { t, i18n } = useTranslation();
   return (
     <div className={classNames(styles.LandingPage)}>
+
       landing page
       <Link to="/register/affected">Register Affected</Link>
       <Link to="/register/mentor">Register Mentor</Link>
       {children}
       <Button>Text</Button>
+      <h1>{t('appLanguage')}</h1>
+      <select defaultValue={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
+        {availableLanguages.map((language) => (
+          <option key={language}>{language}</option>
+        ))}
+      </select>
+      <Button>
+        {t('buttonNext')}
+      </Button>
     </div>
   );
 };
