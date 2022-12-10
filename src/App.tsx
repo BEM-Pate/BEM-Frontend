a10ce8f881e500759f58a2aaf6b5a77fb83c937e
Phillip Kiele <phillipkiele@gmail.com>
2022-12-10 17:21:59 +0100

    Create Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { ReactElement, useCallback } from 'react';
 import './App.module.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import RegisterSeekerPage from './pages/RegisterSeekerPage/RegisterSeekerPage';
 import RegisterPatePage from './pages/RegisterPatePage/RegisterPatePage';
@@ -9,11 +9,24 @@ import Search from './components/Container/Search/Search';
 import Groups from './components/Container/Groups/Groups';
 import Profile from './components/Container/Profile/Profile';
 import Messages from './components/Container/Messages/Messages';
+import LoginPage from './pages/LoginPage/LoginPage';
+import useSessionStorage from './helpers/useSessionStorage';
 
-const App = () => (
-  <BrowserRouter>
+const App = () => {
+  const [userData, setUserData] = useSessionStorage('userData', null);
+
+  const authenticationSwitch = useCallback(
+    (
+      component: ReactElement,
+      redirectPath: string,
+    ) => (userData ? component : <Navigate replace to={redirectPath} />),
+    [userData],
+  );
+
+  return (
     <Routes>
       <Route index element={<LandingPage />} />
+      <Route path="login" element={<LoginPage setUserData={setUserData} />} />
       <Route path="register">
         <Route path="seeker" element={<RegisterSeekerPage />} />
         <Route path="pate" element={<RegisterPatePage />} />
@@ -23,11 +36,11 @@ const App = () => (
           <Route path="search" element={<Search />} />
           <Route path="messages" element={<Messages />} />
           <Route path="groups" element={<Groups />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="profile" element={authenticationSwitch(<Profile />, '/login')} />
         </Route>
       </Route>
     </Routes>
-  </BrowserRouter>
-);
+  );
+};
 
 export default App;
