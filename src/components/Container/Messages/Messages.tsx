2ed5abbbc237fd6cce7a39c41fa387ca481f25b6
Hasan Sivrikaya <35102813+Hasan-1994@users.noreply.github.com>
2023-01-05 22:37:57 +0100

    MessagesPage
    Designen der neuen MessagesPage. Mit Platzhalter für die künfitigen Kontakte und Nachrichten

--- src/components/Container/Messages/Messages.tsx
+++ src/components/Container/Messages/Messages.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import classNames from 'classnames';
+import { useTranslation } from 'react-i18next';
 import styles from './Messages.module.scss';
-import Headline from '../../Headline/Headline';
 
 import placeholder from '../../../images/default.png';
 
-const GERMAN_FIRST_NAMES = ['Anna', 'Bruno', 'Carla', 'Dieter', 'Emma', 'Friedrich', 'Greta', 'Hans', 'Irina', 'Johann'];
-const GERMAN_LAST_NAMES = ['Müller', 'Schmidt', 'Schneider', 'Fischer', 'Weber', 'Meyer', 'Wagner', 'Becker', 'Schulz', 'Hoffmann'];
-const GERMAN_LOCATIONS = ['Berlin', 'Hamburg', 'München', 'Köln', 'Frankfurt', 'Stuttgart', 'Düsseldorf', 'Dortmund', 'Essen', 'Bremen'];
+const GERMAN_FIRST_NAMES = [
+  'Anna',
+  'Bruno',
+  'Carla',
+  'Dieter',
+  'Emma',
+  'Friedrich',
+  'Greta',
+  'Hans',
+  'Irina',
+  'Johann',
+];
+const GERMAN_LAST_NAMES = [
+  'Müller',
+  'Schmidt',
+  'Schneider',
+  'Fischer',
+  'Weber',
+  'Meyer',
+  'Wagner',
+  'Becker',
+  'Schulz',
+  'Hoffmann',
+];
+const GERMAN_LOCATIONS = [
+  'Berlin',
+  'Hamburg',
+  'München',
+  'Köln',
+  'Frankfurt',
+  'Stuttgart',
+  'Düsseldorf',
+  'Dortmund',
+  'Essen',
+  'Bremen',
+];
 const messages = [
   "Hey, how's it going?",
   'Good, just hanging out at home. You?',
   "Same here. Let's plan to grab a coffee",
   "Sounds good to me. Let's pick a day and time.",
-
 ];
 
 const generateRandomPerson = () => {
@@ -43,31 +75,126 @@ const generateRandomPerson = () => {
 
 const paten = generateRandomPerson();
 
-const Messages = () => (
-  <div className={classNames(styles.Messages)}>
-    <Headline headline="h2">Messages</Headline>
-    {
-      paten.map((pate, index) => (
-        <>
-          <div key={index} className={classNames(styles.MessagesItem)}>
-            <img src={placeholder} alt="placeholder" />
-            <div className={classNames(styles.MessagesItemInfo)}>
-              <div className={classNames(styles.MessagesItemInfoName)}>
-                <span>{pate.firstName}</span>
-                <span>{`${pate.date.getDay()}d ago`}</span>
-              </div>
-              <p>
-                {pate.message}
-              </p>
-
-            </div>
-
+const Messages = () => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames(styles.Messages)}>
+      <h1 className={classNames(styles.MessagesHeader)}>
+        {t('messagesPageHeader')}
+      </h1>
+      <h4 className={classNames(styles.MessagesSubheader)}>
+        {t('messagesPageSubHeader')}
+      </h4>
+      <div className={classNames(styles.MessagesContainerContacts)}>
+        {/**
+         * Mappen der Profilbilder
+         * onClick --> öffnen ProfilePage der Kontakte
+         * die jetzigen Bilder dienen nur zur Demonstation des Scrollviews
+         */}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        {' '}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        {' '}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        {' '}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        {' '}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        {' '}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        {' '}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+        {' '}
+        <img
+          src={placeholder}
+          alt="PatePicture"
+          className={classNames(styles.MessagesProfilePictures)}
+        />
+      </div>
+      <h4 className={classNames(styles.MessagesSubheader)}>
+        {t('messagesPageMessages')}
+      </h4>
+      <div
+        className={classNames(styles.MessagesContainerMessages)}
+      >
+        {/**
+         *Bilder durch ProfilePicture ersetzen
+         *die Nachricjten anklickbar machen
+         */
+        paten.map((pate) => (
+          <div className={classNames(styles.MessagesContainerMessagesMessages)}>
+            <img
+              src={placeholder}
+              alt="placeholder"
+              className={classNames(styles.MessagesProfilePictures)}
+            />
+            <span
+              className={classNames(styles.MessagesContainerMessagesTextName)}
+            >
+              {pate.firstName}
+              {' '}
+              {pate.lastName}
+              {' '}
+            </span>
+            <br />
+            <span
+              className={classNames(
+                styles.MessagesContainerMessagesTextMessage,
+              )}
+            >
+              {pate.message}
+            </span>
+            <br />
+            <span
+              className={classNames(styles.MessagesContainerMessagesTextDate)}
+            >
+              {`${pate.date.toDateString()}`}
+            </span>
+            <hr />
           </div>
-          <hr />
-        </>
-      ))
-    }
-  </div>
-);
+        ))
+}
+      </div>
+    </div>
+  );
+};
 
 export default Messages;
