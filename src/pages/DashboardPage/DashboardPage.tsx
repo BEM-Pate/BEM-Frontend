ee23fafb9a92afdfe0526e8bebba3c752a9074f4
Bachdz <xuanbach11696@gmail.com>
2023-01-15 00:56:05 +0100

    Chat Prototype

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import styles from './DashboardPage.module.scss';
 import BottomNavigtionBar from '../../components/BottomNavigationBar/BottomNavigtionBar';
+import { socket, useZustand } from '../../zustand/store';
 
-const DashboardPage = () => (
-  <div className={classNames(styles.Dashboard)}>
+const DashboardPage = () => {
+  const route = useZustand(state => state.route)
+  const location = useLocation()
+  const setChatRoom = useZustand(state => state.setChatroom)
+  useEffect(() => {
+    socket.on('new-message', ({ roomId, messageObj }) => {
+      console.log('heheh')
+      setChatRoom(roomId, messageObj)
+    })
+
+    return () => {
+      socket.off('new-message')
+    }
+  }, [])
+
+  return (<div className={classNames(styles.Dashboard)}
+    style={{ height: '100%' }}
+  >
     <Outlet />
-    <BottomNavigtionBar />
+    {!location.pathname.includes('chat') && <BottomNavigtionBar />}
   </div>
-);
+  )
+};
 
 export default DashboardPage;
