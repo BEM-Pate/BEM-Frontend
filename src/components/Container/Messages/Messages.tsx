be0d13b2b35a63245e5000d4605b5bde63c2d9e8
Bachdz <xuanbach11696@gmail.com>
2023-01-16 22:38:04 +0100

    Fix

--- src/components/Container/Messages/Messages.tsx
+++ src/components/Container/Messages/Messages.tsx
@@ -1,10 +1,10 @@
 import classNames from 'classnames';
-import {useEffect} from 'react';
-import {useTranslation} from 'react-i18next';
-import {useNavigate} from "react-router-dom";
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from "react-router-dom";
 import styles from './Messages.module.scss';
 
-import {useZustand} from '../../../zustand/store';
+import { useZustand } from '../../../zustand/store';
 
 
 const Messages = () => {
@@ -12,26 +12,26 @@ const Messages = () => {
     const navigate = useNavigate();
 
     const [me, chatrooms, fetchChatroom, contacts, setRoute] = useZustand((state) => [state.user, state.chatrooms, state.fetchChatroom, state.contacts, state.setCurrentRoute]);
-    useEffect( () => {
-            fetchChatroom();
-        },
+    useEffect(() => {
+        fetchChatroom();
+    },
         []
     )
     console.log("before sort")
     console.log(chatrooms)
-     // sort by last message time
-    chatrooms.sort((a:any, b:any) => {
+    // sort by last message time
+    chatrooms.sort((a: any, b: any) => {
         let aTime = new Date(a.messages[a.messages.length - 1].time).getTime();
 
         let bTime = new Date(b.messages[b.messages.length - 1].time).getTime();
-        if(aTime > bTime) return -1;
-        if(aTime < bTime) return 1;
+        if (aTime > bTime) return -1;
+        if (aTime < bTime) return 1;
         return 0;
     })
     console.log("after sort")
     console.log(chatrooms)
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     return (
         <div className={classNames(styles.Messages)}>
             <h1 className={classNames(styles.MessagesHeader)}>
@@ -87,25 +87,23 @@ const Messages = () => {
                 }}>
 
                     {
-                        contacts?.map((user: any) => {
-                            const userData = user.baseUserData;
+                        chatrooms?.map((chatRoom: any) => {
+                            let participants = chatRoom.participants;
+                            let userId = participants.find((participant: any) => participant !== me._id);
+                            const user = contacts?.find((user: any) => user._id === userId);
+                            const userData = user?.baseUserData;
                             const b64 = btoa(
                                 userData.avatar.data.data.reduce((data: any, byte: any) => data + String.fromCharCode(byte), '')
                             )
                             const contentType = userData.avatar.contentType;
-                            const chatRoom = chatrooms?.find((chatroom: any) => chatroom.participants.includes(user._id))
                             let unSeenMsgLength = 0;
-
                             chatRoom?.messages.forEach((msg: any) => {
-
                                 const isMeInSeeen = msg.seen?.find((user: any) => user.userId === me._id);
-                                console.log("hehe "+isMeInSeeen)
-                                    if (!isMeInSeeen && msg.sender !== me._id) {
+                                console.log("hehe " + isMeInSeeen)
+                                if (!isMeInSeeen && msg.sender !== me._id) {
                                     unSeenMsgLength++;
                                 }
                             })
-
-
 
                             const lastMessage = chatRoom?.messages[chatRoom.messages.length - 1]
                             const firstName = userData.firstName;
@@ -122,19 +120,19 @@ const Messages = () => {
                                     width: '100%',
                                     padding: '10px'
                                 }}
-                                     onClick={() => {
-                                         navigate(`/dashboard/chatroom/${user._id}`)
-                                         setRoute(`/dashboard/chatroom/${user._id}`)
-                                     }}
+                                    onClick={() => {
+                                        navigate(`/dashboard/chatroom/${user._id}`)
+                                        setRoute(`/dashboard/chatroom/${user._id}`)
+                                    }}
                                 >
                                     <img
                                         src={`data:${contentType};base64,${b64}`}
                                         alt="PatePicture"
                                         className={classNames(styles.MessagesProfilePicturesMsgGroup)}
                                     />
-                                    <div className="msgContainer__msg__content" style={{flex: '1'}}>
-                                        <div className="msgContainer__msg__content__header" style={{display: 'flex'}}>
-                                            <b style={{fontSize:'large'}}> {firstName} {lastName} </b>
+                                    <div className="msgContainer__msg__content" style={{ flex: '1' }}>
+                                        <div className="msgContainer__msg__content__header" style={{ display: 'flex' }}>
+                                            <b style={{ fontSize: 'large' }}> {firstName} {lastName} </b>
 
                                             {unSeenMsgLength > 0 && <span style={
                                                 {
@@ -148,22 +146,103 @@ const Messages = () => {
                                                     marginLeft: 'auto'
                                                 }}>
                                                 {unSeenMsgLength}
-                                        </span>}
+                                            </span>}
 
 
                                         </div>
-                                        <div className="msgContainer__msg__content__body" style={{display: 'flex'}}>
+                                        <div className="msgContainer__msg__content__body" style={{ display: 'flex' }}>
                                             {lastMessage ? (unSeenMsgLength > 0 ?
                                                 <b>{lastMessage.text}</b> : lastMessage.text) : 'No messages yet'}
-                                            <span style={{marginLeft: 'auto'}}
-                                                  className={classNames(styles.MessagesTime)}>{time ? (time.getHours() + ":" + (time.getMinutes() < 10 ? '0' : '') + time.getMinutes()) : ''} {!isToday ? (time.getDate() + "." + ("0" + time.getMonth() + 1).slice(-2)) : ''}</span>
+                                            <span style={{ marginLeft: 'auto' }}
+                                                className={classNames(styles.MessagesTime)}>{time ? (time.getHours() + ":" + (time.getMinutes() < 10 ? '0' : '') + time.getMinutes()) : ''} {!isToday ? (time.getDate() + "." + ("0" + time.getMonth() + 1).slice(-2)) : ''}</span>
                                         </div>
                                     </div>
                                 </div>
 
                             )
 
+
                         })
+
+
+
+                        // contacts?.map((user: any) => {
+                        //     const userData = user.baseUserData;
+                        //     const b64 = btoa(
+                        //         userData.avatar.data.data.reduce((data: any, byte: any) => data + String.fromCharCode(byte), '')
+                        //     )
+                        //     const contentType = userData.avatar.contentType;
+                        //     const chatRoom = chatrooms?.find((chatroom: any) => chatroom.participants.includes(user._id))
+                        //     let unSeenMsgLength = 0;
+
+                        //     chatRoom?.messages.forEach((msg: any) => {
+
+                        //         const isMeInSeeen = msg.seen?.find((user: any) => user.userId === me._id);
+                        //         console.log("hehe "+isMeInSeeen)
+                        //             if (!isMeInSeeen && msg.sender !== me._id) {
+                        //             unSeenMsgLength++;
+                        //         }
+                        //     })
+
+
+
+                        //     const lastMessage = chatRoom?.messages[chatRoom.messages.length - 1]
+                        //     const firstName = userData.firstName;
+                        //     const lastName = userData.lastName;
+                        //     const time = new Date(lastMessage?.time)
+                        //     const todayDate = new Date();
+                        //     const isToday = todayDate.getDate() === time.getDate()
+                        //     return (
+                        //         <div className="msgContainer__msg" style={{
+                        //             borderBottom: '1px solid rgba(34,23,42, .08)',
+                        //             display: 'flex',
+                        //             // justifyContent: 'center',
+                        //             alignItems: 'center',
+                        //             width: '100%',
+                        //             padding: '10px'
+                        //         }}
+                        //              onClick={() => {
+                        //                  navigate(`/dashboard/chatroom/${user._id}`)
+                        //                  setRoute(`/dashboard/chatroom/${user._id}`)
+                        //              }}
+                        //         >
+                        //             <img
+                        //                 src={`data:${contentType};base64,${b64}`}
+                        //                 alt="PatePicture"
+                        //                 className={classNames(styles.MessagesProfilePicturesMsgGroup)}
+                        //             />
+                        //             <div className="msgContainer__msg__content" style={{flex: '1'}}>
+                        //                 <div className="msgContainer__msg__content__header" style={{display: 'flex'}}>
+                        //                     <b style={{fontSize:'large'}}> {firstName} {lastName} </b>
+
+                        //                     {unSeenMsgLength > 0 && <span style={
+                        //                         {
+                        //                             display: 'inline-flex',
+                        //                             height: '25px',
+                        //                             width: '25px',
+                        //                             backgroundColor: '#98C8BC',
+                        //                             borderRadius: '50%',
+                        //                             alignItems: 'center',
+                        //                             justifyContent: 'center',
+                        //                             marginLeft: 'auto'
+                        //                         }}>
+                        //                         {unSeenMsgLength}
+                        //                 </span>}
+
+
+                        //                 </div>
+                        //                 <div className="msgContainer__msg__content__body" style={{display: 'flex'}}>
+                        //                     {lastMessage ? (unSeenMsgLength > 0 ?
+                        //                         <b>{lastMessage.text}</b> : lastMessage.text) : 'No messages yet'}
+                        //                     <span style={{marginLeft: 'auto'}}
+                        //                           className={classNames(styles.MessagesTime)}>{time ? (time.getHours() + ":" + (time.getMinutes() < 10 ? '0' : '') + time.getMinutes()) : ''} {!isToday ? (time.getDate() + "." + ("0" + time.getMonth() + 1).slice(-2)) : ''}</span>
+                        //                 </div>
+                        //             </div>
+                        //         </div>
+
+                        //     )
+
+                        // })
 
                     }
                 </div>
